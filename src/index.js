b1e483ed91701e27546522fc20c9a1f0c3a26554
cronos88 <atlantis9@hotmail.com>
2017-09-25 14:16:18 -0500

    setup react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './containers/App'
+
+// import App from './containers/App'
+import Routes from './routes'
 import registerServiceWorker from './utils/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+)
 registerServiceWorker()
